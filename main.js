b487de6671020681a5e735746047424e912dc161
Gavin Mooallem <gmooallem@gmail.com>
2023-02-15 13:44:02 -0500

    Completed functionality

--- main.js
+++ main.js
@@ -3,20 +3,18 @@ const circles = document.querySelectorAll("circle");
 
 circles.forEach(circle => {
   circle.addEventListener("mouseover", () => {
-    circle.style.stroke = "black";
-    circle.style.strokeWidth = "2px";
+    circle.style.fill = "red";
   });
 
   circle.addEventListener("mouseout", () => {
-    if (!circle.classList.contains("selected")) {
-      circle.style.stroke = "none";
-    }
+    circle.style.fill = "blue";
   });
 
   circle.addEventListener("click", () => {
     if (circle.classList.contains("selected")) {
       circle.classList.remove("selected");
       document.getElementById("coordinates").textContent = "";
+      circle.style.stroke = "none";
     } else {
       circle.classList.add("selected");
       const selectedCircles = document.querySelectorAll(".selected");
@@ -26,7 +24,7 @@ circles.forEach(circle => {
       });
       const x = circle.getAttribute("cx");
       const y = circle.getAttribute("cy");
-      document.getElementById("coordinates").textContent += `(${x / 60 | 0}, ${10 - y / 60 | 0}) `;
+      document.getElementById("coordinates").textContent = `(${x / 60 | 0}, ${10 - y / 60 | 0}) `;
     }
   });
 });
@@ -42,27 +40,25 @@ form.addEventListener("submit", event => {
 
   if (x >= 1 && x <= 9 && y >= 1 && y <= 9) {
     const newCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    newCircle.setAttribute("cx", x * 60);
-    newCircle.setAttribute("cy", (10 - y) * 60);
+    newCircle.setAttribute("cx", (x * 50) + 50);
+    newCircle.setAttribute("cy", 550 - (50*y));
     newCircle.setAttribute("r", "8");
     newCircle.setAttribute("class", "point");
     scatterplot.appendChild(newCircle);
 
     newCircle.addEventListener("mouseover", () => {
-      newCircle.style.stroke = "black";
-      newCircle.style.strokeWidth = "2px";
+      newCircle.style.fill = "red";
     });
-
+  
     newCircle.addEventListener("mouseout", () => {
-      if (!newCircle.classList.contains("selected")) {
-        newCircle.style.stroke = "none";
-      }
+      newCircle.style.fill = "blue";
     });
 
     newCircle.addEventListener("click", () => {
       if (newCircle.classList.contains("selected")) {
         newCircle.classList.remove("selected");
         document.getElementById("coordinates").textContent = "";
+        newCircle.style.stroke = "none";
       } else {
         newCircle.classList.add("selected");
         const selectedCircles = document.querySelectorAll(".selected");
